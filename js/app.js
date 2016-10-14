6cee9568f11a88d70659442d80fe3248568764ea
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-14 14:11:13 -0700

    incorporate joel's feedback
    added a new map problem. Adapted the old map problem into a foreach problem because that was more appropriate.
    Problem6 still needs a solution to be written.
    all function names are now in lowercase
    problem6 is now meant to have the map be built dynamically
    problem5 now assumes that problem2 will be called before it as a precondition

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
-/* Here is the situation:
+/* Problem #1 - Fizz Buzz!
+Fizz buzz is a classic programming problem that commonly gets used as a litmus test for progammers.
+
+Write a function (Problem5) that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+*/
+function problem1() {
+    for (var i=1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+/* Here is the situation for problems 2-5:
 
 Tori's student records got damaged in the storm last weekend. All that survided the storm was a javascript list of objects.
 The objects in this list represent uw info students. Somehow, the effects of the storm compromised the integrity of the iSchool
 student records. It is damaged, and she needs your help to fix it!
-
-Note: For each problem, you will need to do the following:
-    * Log your output to the console with console.log()
-    * Return your output at the end of your method
 */
 
 // This is all that is left of the data!
@@ -37,7 +54,7 @@ var students = [
         gpa: 12.25,
     },
     {
-        name: 'Beyonce',
+        name: 'Beyonce Knowles',
         graduating: false,
         gpa: 13.69,
     },
@@ -45,53 +62,60 @@ var students = [
 ];
 
 
-/* Problem #1 - map() practice https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+/* Problem #2 - Foreach practice
 
 Unfortunately, all of the student's GPAs somehow got squared (gpa * gpa).
-Use the map() function with an anonymous callback to quickly fix the student GPA records by taking their square root.
+Use a foreach loop to fix the student GPAs by taking their square root.
 
 Notes:
     * Javascript has a built in square root function. Look up how to use it!
     * Your function should actually update the gpa values in the students array.
-    * Your function should also return a copy of the updated list of students with their corrected GPAs.
 */
-function Problem1() {
-    var roots = students.map(function(student, index, array) {
-        array[index].gpa = Math.sqrt(student.gpa); // update students array
-        return array[index]; // add item to roots array
+function problem2() {
+    students.forEach(function(student) {
+        student.gpa = Math.sqrt(student.gpa);
     });
-    return roots;
 }
 
-/* Problem #2 - filter() practice https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+/* Problem #3 - map/filter/reduce practice
+
+Tori needs a list of all of the info student's last names.
+Write a function that returns a string array containing all of the student's last names.
+*/
+
+function problem3() {
+    return students.map(function(student) {
+        return student.name.split(' ')[1];
+    });
+}
+
+/* Problem #4 - map/filter/reduce practice
 
 Tori has a list of all students, but she also needs a list that represents the subset of students who are graduating this year.
-Use the map() function with an anonymous callback to help her get her list!
+Write a method that returns a list of students who are graduating this year.
 */
-function Problem2() {
+function problem4() {
     var graduatingStudents = students.filter(function(student) {
         return student.graduating;
     });
-    console.log(graduatingStudents);
     return graduatingStudents;
 }
 
-/* Problem #3 - reduce() practice https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Syntax
+/* Problem #5 - map/filter/reduce practice
 
 Tori needs your help computing the average GPA of info students.
 Use the reduce() function to compute the average GPA of info students.
 
-Note: You will need to first feed in the results of the Problem1() method to Problem3(), or else your average GPA will be squared!
+Note: You will need to run problem2() before you run this function, otherwise your result will be squared!
 */
-function Problem3() {
-    var rootedGPAs = Problem1();
-    var cumGPA = rootedGPAs.reduce(function(left, right) {
-        return {gpa: left.gpa + right.gpa};
+function problem5() {
+    var cumGPA = students.reduce(function(total, current) {
+        return {gpa: total.gpa + current.gpa};
     });
     return cumGPA.gpa / students.length;
 }
 
-/* Problem #4
+/* Problem #6
 
 Tori needs to know which letter of the alphabet is the most common first letter among all the iSchool student names.
 Help her out!
@@ -101,42 +125,8 @@ Example: If Cameron was the only student in the iSchool, then c=1 and all the ot
 Note:
     * You will see a similar problem pattern on your homework
 */
-function Problem4() {
-    var frequency = {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: 0,
-        g: 0,
-        h: 0,
-        i: 0,
-        j: 0,
-        k: 0,
-        l: 0,
-        m: 0,
-        n: 0,
-        p: 0,
-        q: 0,
-        r: 0,
-        s: 0,
-        t: 0,
-        u: 0,
-        v: 0,
-        w: 0,
-        x: 0,
-        y: 0,
-        z: 0
-    };
+function problem6() {
 
 }
 
-/* Problem #5 - Fizz Buzz!
-Fizz buzz is a classic programming problem that commonly gets used as a litmus test for progammers.
 
-Write a function (Problem5) that prints the numbers from 1 to 100.
-But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
-For numbers which are multiples of both three and five print "FizzBuzz".
-
-*/
