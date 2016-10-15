bb700740384b23beb8a763ed5f4afbea8d5b0b61
Joel Ross <joelross@uw.edu>
2016-10-14 18:53:14 -0700

    Update app.js
    Clarify instructions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 /* Problem #1 - Fizz Buzz!
 Fizz buzz is a classic programming problem that commonly gets used as a litmus test for progammers.
 
-Write a function (Problem5) that prints the numbers from 1 to 100.
+Write a function (problem1) that prints the numbers from 1 to 100.
 But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
 For numbers which are multiples of both three and five print "FizzBuzz".
 */
 function problem1() {
     console.log("Hello, world!");
 }
+
 
 /* Here is the situation for problems 2-5:
 
@@ -52,10 +53,10 @@ var students = [
 ];
 
 
-/* Problem #2 - Foreach practice
+/* Problem #2 - forEach() practice
 
 Unfortunately, all of the student's GPAs somehow got squared (gpa * gpa).
-Use a foreach loop to fix the student GPAs by taking their square root.
+Use the forEach() function to fix the student GPAs by taking their square root.
 
 Notes:
     * Javascript has a built in square root function. Look up how to use it!
@@ -66,34 +67,45 @@ Notes:
 /* Problem #3 - map/filter/reduce practice
 
 Tori needs a list of all of the info student's last names.
-Write a function that returns a string array containing all of the student's last names.
+Write a function `problem3()` that uses either map(), filter(), or reduce() to return an array containing all of the student's 
+last names.
 */
 
 
 /* Problem #4 - map/filter/reduce practice
 
 Tori has a list of all students, but she also needs a list that represents the subset of students who are graduating this year.
-Write a method that returns a list of students who are graduating this year.
+Write a function `problem4()` that uses either map(), filter(), or reduce() to return a list of students who are graduating this 
+year.
 */
+
 
 /* Problem #5 - map/filter/reduce practice
 
 Tori needs your help computing the average GPA of info students.
-Use the reduce() function to compute the average GPA of info students.
+Write a function that uses either map(), filter(), or reduce() to compute and return the average GPA of info students.
 
 Note: You will need to run problem2() before you run this function, otherwise your result will be squared!
 */
 
+
 /* Problem #6
 
-Tori needs to know which letters from student names are the most common.
-Use an associative array (also known as a map, dictionary, or hash table) to record the frequency of each character that you encounter in uw info student names.
-Return the frequency map (the associative array) when you are done.
+Tori needs to know which letters from student names are the most common (for printing purposes).
+Use an Object (aka an associative array, map, dictionary, or hash table) to record the frequency of each character 
+used the in uw info student names.
+Return the frequency map (the object) when you are done.
 
 Note:
-    * You will see a similar problem pattern on your homework
-    * If a letter isn't used by any of the student names, you don't need to include it. In other words, you can build this map dynamically.
+    * You will see a similar problem pattern to this on your homework!
+    * If a letter isn't used by any of the student names, you don't need to include it. In other words, you can build this map 
+      dynamically.
     * Don't worry about the spaces in between the first and last names
 */
 
 
+/* Bonus problem
+
+Can you log out a histogram of the letter frequencies?
+
+*/
